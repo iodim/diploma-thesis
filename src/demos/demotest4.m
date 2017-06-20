1e262799d5dc473a50c6d0cf8a5989d9da985a33
iodim <ioanndima@gmail.com>
2017-06-20 17:54:45 +0300

    modified some experiments

--- src/demos/demotest4.m
+++ src/demos/demotest4.m
@@ -7,23 +7,38 @@ x0 = ones(n, 1);
 z0 = zeros(n,1);
 q0 = [x0; z0];
 
-k = 1;
-satlvl = 1 - 1e-16;
-mu = 0.01;
+k = 4;
+mu = 0.001;
 alpha = [4, 6, 4, 1];
 
-r = [1.1, 1.2, 1.3];
+% r = [1.1, 1.2, 1.3];
+r = [2 2 2];
 Lambda = fliplr(poly(-r))';
 
-rho = @(t) (20 - 0.05)*exp(-1*t) + 0.05;
+rho = @(t) (30 - 0.05)*exp(-2*t) + 0.05;
 
-plant = @plant4b;
+plant = @plant4c;
 
-tmax = 15;
+tmax = 10;
 peaking_time = 0.05;
-ode_options = odeset('AbsTol', 1e-6, 'RelTol', 1e-3);
+ode_options = odeset('AbsTol', 1e-9, 'RelTol', 1e-6);
 
-%% High-Gain Observer
+%% Full state feedback
+controller = @(t, x, w) ppc(t, x, Lambda, rho, k);
+sys1 = @(t, q) control_loop(t, q, plant, [n 0 0], controller);
+
+[t, q] = ode15s(sys1, [0 tmax], x0, ode_options);
+
+% Reconstruct sliding surface, its estimate, and the control input
+x = q(:, 1:n);
+
+s = x*Lambda;
+u = controller(t, x');
+
+plotter4(t, x, s, rho, u);
+
+%% Output feedback (PPC-Sigma w/ HGO)
+satlvl = 1 - 1e-4;
 observer = @(t, xhat, y) hgo(t, xhat, y, alpha, mu);
 controller = @(t, x, w) ppc_sigma(t, x, Lambda, rho, k, satlvl);
 sys1 = @(t, q) control_loop(t, q, plant, [n 0 n], controller, observer);
@@ -39,52 +54,38 @@ s = x*Lambda;
 shat = xhat*Lambda;
 u = controller(t, xhat');
 
-%% Plots
-figure();
-subplot(2, 2, 1)
-    box on; hold on; axis([0, max(t), 1.1*min(x(:, 1)), 1.1*max(x(:, 1))]);
-    plot(t, x(:, 1), 'k');
-    plot(t, xhat(:, 1), '--k');
-    ylabel('$x_1(t), \hat{x}_1(t)$', 'Interpreter', 'Latex');
-subplot(2, 2, 2)
-    box on; hold on; axis([0, max(t), 1.1*min(x(:, 2)), 1.1*max(x(:, 2))]);
-    plot(t, x(:, 2), 'k');
-    plot(t, xhat(:, 2), '--k');
-    ylabel('$x_2(t), \hat{x}_2(t)$', 'Interpreter', 'Latex');    
-subplot(2, 2, 3)
-    box on; hold on; axis([0, max(t), 1.1*min(x(:, 3)), 1.1*max(x(:, 3))]);
-    plot(t, x(:, 3), 'k');
-    plot(t, xhat(:, 3), '--k');
-    ylabel('$x_3(t), \hat{x}_3(t)$', 'Interpreter', 'Latex');
-    xlabel('$t$', 'Interpreter', 'Latex');
-subplot(2, 2, 4)
-    box on; hold on; axis([0, max(t), 1.1*min(x(:, 4)), 1.1*max(x(:, 4))]);
-    plot(t, x(:, 4), 'k');
-    plot(t, xhat(:, 4), '--k');   
-    ylabel('$x_4(t), \hat{x}_4(t)$', 'Interpreter', 'Latex');
-    xlabel('$t$', 'Interpreter', 'Latex');
+plotter4(t, x, s, rho, u, xhat, shat, t_p);
+if any(find(abs(s./rho(t)) >= 1))
+    fprintf('PPC-Sigma w/ HGO:\n\t Surface violated perfomance.\n');
+else
+    fprintf('PPC-Sigma w/ HGO:\n\t Surface did not violate perfomance.\n');
+end
 
-figure();
-subplot(2, 1, 1)
-    box on; hold on; axis([0, max(t), -1.1*rho(0), 1.1*rho(0)]);
-    plot(t, s, 'k');
-    plot(t, shat, '--k');
-    plot([t, t], [rho(t), -rho(t)], ':k'); 
-    ylabel('$s(x(t)), s(\hat{x}(t))$', 'Interpreter', 'Latex');
-    % peaking plot
-%     axes('position', [.675 .675 .2 .2]); 
-%     box on; hold on; axis tight;
-%     plot(t(t_p), s(t_p), 'k');
-%     plot(t(t_p), shat(t_p), '--k');
-%     plot([t(t_p), t(t_p)], [rho(t(t_p)), -rho(t(t_p))], ':k'); 
+%% Output feedback (PPC-Sat w/ HGO)
+satlvl = 46.61;
+observer = @(t, xhat, y) hgo(t, xhat, y, alpha, mu);
+controller = @(t, x, w) ppc_sat(t, x, Lambda, rho, k, satlvl);
+sys1 = @(t, q) control_loop(t, q, plant, [n 0 n], controller, observer);
 
-subplot(2, 1, 2)
-    box on;
-    hold on;
-    plot(t, u, 'k')
-    ylabel('$u(t)$', 'Interpreter', 'Latex');
-    xlabel('$t$', 'Interpreter', 'Latex');
-    % peaking plot
-%     axes('position', [.675 .175 .2 .2]); 
-%     box on; hold on; axis([0, max(t(t_p)), -1.1*sat, 1.1*sat]);
-%     plot(t(t_p), u(t_p), 'k')
+[t, q] = ode15s(sys1, [0 tmax], q0, ode_options);
+t_p = (t < peaking_time);
+
+% Reconstruct sliding surface, its estimate, and the control input
+x = q(:, 1:n);
+xhat = q(:, n+1:end);
+
+s = x*Lambda;
+shat = xhat*Lambda;
+u = controller(t, xhat');
+
+plotter4(t, x, s, rho, u, xhat, shat, t_p);
+if any(find(abs(s./rho(t)) >= 1))
+    fprintf('PPC-Sat w/ HGO:\n\t Surface violated perfomance.\n');
+else
+    fprintf('PPC-Sat w/ HGO:\n\t Surface did not violate perfomance.\n');
+end
+
+
+close(figure(1));
+close(figure(3));
+close(figure(5));
--- src/demos/observer_demo.m
+++ src/demos/observer_demo.m
@@ -15,7 +15,7 @@ muV = [1 1 1 1];
 lambda = [2 2 3 4];
 
 % HGO parameters
-mu = 0.01;
+mu = 0.001;
 alpha = [4, 6, 4, 1];
 
 % General parameters
@@ -29,8 +29,8 @@ tmax = 15;
 ode_options = odeset('AbsTol', 1e-9, 'RelTol', 1e-6);
 
 %% Robust Exact Differentiator (RED)
-% observer = @(t, xhat, y) gfed(t, xhat, y, lambda, L, muV);
-observer = @(t, xhat, y) hgo(t, xhat, y, alpha, mu);
+observer = @(t, xhat, y) gfed(t, xhat, y, lambda, L, muV);
+% observer = @(t, xhat, y) hgo(t, xhat, y, alpha, mu);
 controller = @(t, x, w) nocontrol();
 sys = @(t, q) control_loop(t, q, plant, [n 0 n], controller, observer);
 
@@ -39,7 +39,7 @@ sys = @(t, q) control_loop(t, q, plant, [n 0 n], controller, observer);
 x = q(:, 1:n);
 xhat = q(:, n+1:end);
 
-%% Plots
+% Plots
 figure;
 subplot(2, 2, 1)
     box on; hold on; axis([0, max(t), 1.1*min(x(:, 1)), 1.1*max(x(:, 1))]);
@@ -63,3 +63,40 @@ subplot(2, 2, 4)
     plot(t, xhat(:, 4), '--k');   
     ylabel('$x_4(t), \hat{x}_4(t)$', 'Interpreter', 'Latex');
     xlabel('$t$', 'Interpreter', 'Latex');
+
+    
+%% High-Gain Observer (HGO)
+observer = @(t, xhat, y) hgo(t, xhat, y, alpha, mu);
+controller = @(t, x, w) nocontrol();
+sys = @(t, q) control_loop(t, q, plant, [n 0 n], controller, observer);
+
+[t, q] = ode15s(sys, [0 tmax], [q0; qhat0], ode_options);
+
+x = q(:, 1:n);
+xhat = q(:, n+1:end);
+
+% Plots
+figure;
+subplot(2, 2, 1)
+    box on; hold on; axis([0, max(t), 1.1*min(x(:, 1)), 1.1*max(x(:, 1))]);
+    plot(t, x(:, 1), 'k');
+    plot(t, xhat(:, 1), '--k');
+    ylabel('$x_1(t), \hat{x}_1(t)$', 'Interpreter', 'Latex');
+subplot(2, 2, 2)
+    box on; hold on; axis([0, max(t), 1.1*min(x(:, 2)), 1.1*max(x(:, 2))]);
+    plot(t, x(:, 2), 'k');
+    plot(t, xhat(:, 2), '--k');
+    ylabel('$x_2(t), \hat{x}_2(t)$', 'Interpreter', 'Latex');    
+subplot(2, 2, 3)
+    box on; hold on; axis([0, max(t), 1.1*min(x(:, 3)), 1.1*max(x(:, 3))]);
+    plot(t, x(:, 3), 'k');
+    plot(t, xhat(:, 3), '--k');
+    ylabel('$x_3(t), \hat{x}_3(t)$', 'Interpreter', 'Latex');
+    xlabel('$t$', 'Interpreter', 'Latex');
+subplot(2, 2, 4)
+    box on; hold on; axis([0, max(t), 1.1*min(x(:, 4)), 1.1*max(x(:, 4))]);
+    plot(t, x(:, 4), 'k');
+    plot(t, xhat(:, 4), '--k');   
+    ylabel('$x_4(t), \hat{x}_4(t)$', 'Interpreter', 'Latex');
+    xlabel('$t$', 'Interpreter', 'Latex');
+    
