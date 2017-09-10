e585b752e2ebebd002128fbf9c54a9e65a4a4105
svart <gorgothian@yahoo.com>
2017-09-10 20:30:45 +0300

    added create_rho.m in util

---
+++ src/util/create_rho.m
@@ -0,0 +1,15 @@
+function [rho_wrapper] = create_rho(rho0, t1, ss)
+    rho_wrapper = @(t) rho_exp(rho0, t1, ss, t);
+end
+
+function [y] = rho_exp(rho0, t1, ss, t)
+    lambda = -(lambertw(-1, -ss/(rho0*exp(1))) + 1)/t1;
+    rho1 = rho0.*exp(-lambda.*t) + lambda.*rho0.*exp(-lambda.*t1).*t;
+    y = (t <= t1).*rho1 + (t > t1).*ss;
+end
+
+function [y] = rho_inv(t1, ss, t)
+    c = ss*t1/2;
+    rho1 = c./t + c./(t1.^2).*t;
+    y = (t <= t1).*rho1 + (t > t1).*ss;
+end
